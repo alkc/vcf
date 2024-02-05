b0a8f46fbe203d0da797d2fb746acb67d9ab9519
Alexander Koc <alexander.koc@skane.se>
2024-02-05 14:17:12 +0100

    Add VCF field enum thingie

--- vcf.py
+++ vcf.py
@@ -4,8 +4,20 @@ import click
 import sys
 
 
+class VCF_FIELDS:
+    CHROM = 0
+    POS = 1
+    ID = 2
+    REF = 3
+    ALT = 4
+    QUAL = 5
+    FILTER = 6
+    INFO = 7
+    FORMAT = 8
+
 class INFO_FIELDS:
     RANK_SCORE = "RankScore"
+    CADD_SCORE = "CADD"
 
 
 @click.group()
