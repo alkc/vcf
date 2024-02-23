24e33744beb903e2bd61de7ef9ef53680dafa324
Alexander Koc <alexander.koc@skane.se>
2024-02-23 12:33:45 +0100

    Remove unused imports

--- compare-rankscore.py
+++ compare-rankscore.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
 
 import gzip
-import click
-import tempfile
-import os
 from collections import defaultdict
 from typing import Generator
+
+import click
 
 RANK_SCORE_KEY = "RankScore="
 RANK_SCORE_KEY_LEN = len(RANK_SCORE_KEY)
@@ -17,7 +16,6 @@ def open_vcf(path_to_vcf: str) -> Generator:
     """
 
     def _vcf_file_generator():
-
         if path_to_vcf.endswith(".gz"):
             with gzip.open(path_to_vcf, "rt") as vcf_handle:
                 for line in vcf_handle:
@@ -36,7 +34,6 @@ def get_id_fields(vcf_row):
 
 
 def process_vcf_into_scores(vcf) -> dict[tuple, int]:
-
     scores = {}
 
     for line in vcf:
@@ -78,12 +75,7 @@ def compare_rank_score(vcf_file1: str, vcf_file2: str | None = None):
 
 
 def _get_rankscore(line: str) -> int:
-
-    score = (
-        line[line.find(RANK_SCORE_KEY) + RANK_SCORE_KEY_LEN :]
-        .split(":")[1]
-        .split(";")[0]
-    )
+    score = line[line.find(RANK_SCORE_KEY) + RANK_SCORE_KEY_LEN :].split(":")[1].split(";")[0]
     return int(score)
 
 
