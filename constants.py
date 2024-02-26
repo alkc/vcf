878eb932eab27503b94aeaac210357c856efeaa5
Alexander Koc <alexander.koc@skane.se>
2024-02-26 10:33:14 +0100

    Add constants

---
+++ constants.py
@@ -0,0 +1,15 @@
+class VCF_FIELDS:
+    CHROM = 0
+    POS = 1
+    ID = 2
+    REF = 3
+    ALT = 4
+    QUAL = 5
+    FILTER = 6
+    INFO = 7
+    FORMAT = 8
+
+
+class INFO_FIELDS:
+    RANK_SCORE = "RankScore"
+    CADD_SCORE = "CADD"
