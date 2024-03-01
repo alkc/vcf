e391e590b06141a7dbc2056185051c9077b67ae9
Alexander Koc <alexander.koc@skane.se>
2024-03-01 09:48:29 +0100

    Add suffix opt to loading bar

--- util.py
+++ util.py
@@ -1,7 +1,7 @@
 import sys
 
 
-def print_percent_done(index, total, bar_len=None, title="Please wait"):
+def print_percent_done(index, total, bar_len=None, title="Please wait", suffix=""):
     """
     index is expected to be 0 based index.
     0 <= index < total
@@ -21,7 +21,11 @@ def print_percent_done(index, total, bar_len=None, title="Please wait"):
     done_str = "█" * int(done)
     togo_str = "░" * int(togo)
 
-    print(f"\t⏳{title}: [{done_str}{togo_str}] {percent_done}% done", end="\r", file=sys.stderr)
+    print(
+        f"\t⏳{title}: [{done_str}{togo_str}] {percent_done}% done. {suffix}",
+        end="\r",
+        file=sys.stderr,
+    )
 
     if index == total:
         print("\t✅", file=sys.stderr)
